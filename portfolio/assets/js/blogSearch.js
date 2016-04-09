5e38a082fe5a2d7f0c3cf705aa3b5d3357506b24
motlj <joshuadamotl@gmail.com>
2016-04-09 15:22:07 -0500

    changed search to search throuhg posts only

--- portfolio/assets/js/blogSearch.js
+++ portfolio/assets/js/blogSearch.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         var filter = $(this).val(), count = 0;
  
         // Loop through the comment list
-        $("div").each(function(){
+        $('div id="blog"').each(function(){
  
             // If the list item does not contain the text phrase fade it out
             if ($(this).text().search(new RegExp(filter, "i")) < 0) {

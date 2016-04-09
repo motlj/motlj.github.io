619de29e08bc440cd1d528997b85244725c02662
motlj <joshuadamotl@gmail.com>
2016-04-09 14:46:17 -0500

    attempting to add search

---
+++ portfolio/assets/js/blog.js
@@ -0,0 +1,17 @@
+    function request() {
+      return $.ajax({
+        type : 'GET',
+        dataType : 'json',
+        cache : 'false',
+        url : "blogPosts.json",
+        success : function(response) {
+          $('#blog').append('<div class="row">');
+          $.each(response.posts, function(key, value){
+            console.log(value.postTitle);
+            $('#blog').append('<div class="col-lg-7"><h3>' + value.postTitle + '</h3><h5>' + value.postDate + '</h5><p>' + value.postContent + '</p><hr class="featurette-divider"></div>');
+          });
+          $('#blog').append('</div>');
+        }
+      })
+    }
+    $(document).ready(request);

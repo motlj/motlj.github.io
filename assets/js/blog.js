b785eb96b4b33f57fb9ddf00514b818380d37af1
motlj <joshuadamotl@gmail.com>
2016-05-26 06:18:20 -0500

    attempting a different kind of pagination

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,14 +6,15 @@ function request() {
     url : "blogPosts.json",
     success : function(response) {
       //$('#blog').append('<div class="row">');
-      $('#blog').append('<ul id="list">');
+      //$('#blog').append('<ul id="list">');
       $.each(response.posts, function(key, value){
         console.log(value.postTitle);
-        $('#blog').append('<li><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></li>');
+        $('#blog').append('<section><figure>' + value.postTitle + '</figure></section>');
         //$('#blog').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
       });
-      $('#blog').append('</ul>');
+      //$('#blog').append('</ul>');
     }
-  })
+  });
+  $(document).ready(request);
 }
 $(document).ready(request);

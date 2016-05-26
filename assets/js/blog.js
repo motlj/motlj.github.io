8d577cc5d0f2ff5f3f7b0bd6732ff3a63b2b6d81
motlj <joshuadamotl@gmail.com>
2016-05-26 06:22:13 -0500

    reverted back to blog without pagination

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,16 +5,13 @@ function request() {
     cache : 'false',
     url : "blogPosts.json",
     success : function(response) {
-      //$('#blog').append('<div class="row">');
-      //$('#blog').append('<ul id="list">');
+      $('#blog').append('<div class="row">');
       $.each(response.posts, function(key, value){
         console.log(value.postTitle);
-        $('#blog').append('<section><figure><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '</p></figure></section>');
-        //$('#blog').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
+        $('#blog').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
       });
-      //$('#blog').append('</ul>');
+      $('#blog').append('</div>');
     }
-  });
-  $(document).ready(request);
+  })
 }
 $(document).ready(request);

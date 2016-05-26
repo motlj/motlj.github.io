faadb5c1d1a89d65e9fad9473795e6719f34da44
motlj <joshuadamotl@gmail.com>
2016-05-26 06:07:50 -0500

    added missing $

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -9,7 +9,7 @@ function request() {
       $('#blog').append('<ul id="list">');
       $.each(response.posts, function(key, value){
         console.log(value.postTitle);
-        ('#blog').append('<li><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></li>');
+        $('#blog').append('<li><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></li>');
         //$('#blog').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
       });
       $('#blog').append('</ul>');

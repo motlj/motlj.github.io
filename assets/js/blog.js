544df01e3f16c37127279d2c9d88c8d955da1cb5
motlj <joshuadamotl@gmail.com>
2016-06-07 19:55:13 -0500

    attempted to move pagination to bottom of blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,12 +5,12 @@ function request() {
     cache : 'false',
     url : "blogPosts.json",
     success : function(response) {
-      $('#light-pagination').append('<div class="row">');
+      $('#blog').append('<div class="row">');
       $.each(response.posts, function(key, value){
         console.log(value.postTitle);
-        $('#light-pagination').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
+        $('#blog').append('<div class="col-lg-12"><p><large><strong>' + value.postTitle + '</strong></large><br><em>' + value.postDate + '</em><br>' + value.postContent + '<br><br><br><br></p></div>');
       });
-      $('#light-pagination').append('</div>');
+      $('#blog').append('</div>');
     }
   })
 }
